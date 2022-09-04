aeb5d2a10aaca9fbf392d1aabc33ae9895b0a060
koby <kobybitonu@gmail.com>
2022-09-04 22:06:34 +0300

    fix product component

--- components/ProductGrid.vue
+++ components/ProductGrid.vue
@@ -2,7 +2,7 @@
   <b-container>
     <b-form-row style="padding: 5px 0">
       <b-col v-for="p of products" :key="p.id" md="3" style="padding: 5px">
-        <Product :product="p" @click="$emit('update:product', p)" @edit="$emit('edit', p.id)" />
+        <Product :product="p" @edit="$emit('edit', p.id)" />
       </b-col>
     </b-form-row>
   </b-container>
